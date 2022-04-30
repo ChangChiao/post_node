7f35830e3d5dc3c4c9994d75e57a1cf8c56133c1
joe <joe.chang1014@gmail.com>
2022-04-30 15:56:32 +0800

    fix patchBug

--- logic/patchPost.js
+++ logic/patchPost.js
@@ -2,22 +2,28 @@ const { successHandle, errorHandle } = require('../handler');
 const Post = require('../models/posts');
 
 const patchPost = async(req, res, body) => {
-	const id = req.url.split('/').pop();
-	const { name, content } = JSON.parse(body);
-	if (content === undefined && name === undefined) {
-		errorHandle(res, '參數有缺');
-		return;
-	}
 	try {
+		const id = req.url.split('/').pop();
+		const { name, content } = JSON.parse(body);
+		if (content === undefined || name === undefined) {
+			errorHandle(res, '參數有缺');
+			return;
+		}
 		// eslint-disable-next-line no-unused-vars
-		await Post.findByIdAndUpdate(id, {
+		Post.findByIdAndUpdate(id, {
 			name,
 			content,
+		}, {new: true}, function(err, model){
+			if(!model){
+				errorHandle(res, '無此id');
+			}else{
+				successHandle(res, model);
+			}
 		});
-		const target = await Post.findById(id)
-		successHandle(res, target);
+		// const target = await Post.findById(id)
+		// successHandle(res, target);
 	} catch (error) {
-		errorHandle(res, '無此ID');
+		errorHandle(res, '修改失敗!');
 	}
 };
 
